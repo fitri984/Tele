65be8c611845ccc043740f50ad699cd3df32af8c
fitri984 <fitriamelia5.bbb@gmail.com>
2024-12-10 00:38:55 +0700

    Update undefined.js

--- undefined.js
+++ undefined.js
@@ -1,8 +1,8 @@
 const global = {
-    superUser: [6555756895, 5126860596], // id mu, bisa lebih dari 1 id
+    superUser: [6531090636], // id mu, bisa lebih dari 1 id
     usernameOwner: "@devbeal",
     botName: "Undercrasher_bot",
-    botToken: "7690163438:AAG1_PyKLDLUGwLmcH3AdO2TnIsqSmEWAtQ", // token bot mu
+    botToken: "7386689699:AAGgd2JV0hshtYKEdUiBv37MNlT8iLOvi-w", // token bot mu
     channelLink: "https://t.me/NullxTeam",
     preview: 'https://j.top4top.io/m_32448eu0w1.mp4'
 }
